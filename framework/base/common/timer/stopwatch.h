6648d3e5e4af384c951224b15d57a7f40991ff77
Michal Lichojwan <mlichojwan@vewd.com>
2019-01-11 14:27:16 +0100

    Squash stopwatch classes into 2 classes
    CStopWatch and CHighResStopWatch classes handles time measure.
    Could be compiled using different libraries for different systems.

--- framework/base/common/timer/stopwatch.h
+++ framework/base/common/timer/stopwatch.h
@@ -7,121 +7,59 @@
 #ifndef __STOPWATCH_H__
 #define __STOPWATCH_H__
 
-//#define WINTIME_MEASURE
-//#define CLIBTIME_MEASURE
-#define STDCHRONOLIB_TIME_MEASURE
 
-#if defined WINTIME_MEASURE
-#include <windows.h>
-#endif //WINTIME_MEASURE
+//CLIBTIME_MEASURE
+//#define STDCHRONOLIB_TIME_MEASURE
+//#define WINTIME_MEASURE
 
 #if defined STDCHRONOLIB_TIME_MEASURE
+
 #include <chrono>
-#endif //STDCHRONOLIB_TIME_MEASURE
+#define MILLISECS_TIME std::chrono::steady_clock::time_point
+#define MICROSECS_TIME std::chrono::high_resolution_clock::time_point
 
-/*
-#if defined WINTIME_MEASURE
-//#include <windows.h>
-//#include <MMSystem.h>
+#elif defined WINTIME_MEASURE
 
-#define CLOCK_TIME unsigned int
-#define MICROSEC_TIME __int64
-
-#elif defined CHRONOLIB_TIME_MEASURE
-#include <chrono>
-#define CLOCK_TIME std::chrono::steady_clock::time_point
-#define MICROSEC_TIME std::chrono::steady_clock::time_point
-//#define CLOCK_TIME std::chrono::milliseconds;
-//#define MICROSEC_TIME std::chrono::microseconds;
+#include <windows.h>
+#include <MMSystem.h>
+#define MILLISECS_TIME DWORD
+#define MICROSECS_TIME __int64
 
 #else
+
 #include <time.h>
-#define CLOCK_TIME clock_t
-#define MICROSEC_TIME clock_t
+#define MILLISECS_TIME clock_t
+#define MICROSECS_TIME clock_t
 
 #endif // time measure variables
-*/
 
-// Low Res timer
-#if defined WINTIME_MEASURE
 /////////////////////////////////////////////////////////////////////////////////////////////
-class CStopWatch
-{
-public:
-    CStopWatch(bool activate=true);
-    ~CStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsPaused() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    DWORD m_StartTime;
-    DWORD m_CurrTime;
-
-    bool m_Paused;
-};
-/////////////////////////////////////////////////////////////////////////////////////////////
-#elif defined STDCHRONOLIB_TIME_MEASURE
-/////////////////////////////////////////////////////////////////////////////////////////////
-class CStopWatch
-{
-public:
-    CStopWatch(bool activate=true);
-    ~CStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsPaused() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    std::chrono::steady_clock::time_point m_StartTime;
-    std::chrono::steady_clock::time_point m_CurrTime;
-
-    bool m_Paused;
-};
-/////////////////////////////////////////////////////////////////////////////////////////////
-#else // Use C time library
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-#include <time.h>
-
+// Low res stopwatch that gives time in milliseconds
 class CStopWatch
 {
 public:
     CStopWatch(bool bactivate=true);
     ~CStopWatch();
-
+    
     void Start();
     void Stop();
     void Reset();
-
+    
     bool IsPaused() const;
-
+    
     float GetTime();
     unsigned int GetTicks();
-
+    
 protected:
-    clock_t m_StartTime;
-    clock_t m_CurrTime;
-
+    MILLISECS_TIME m_StartTime;
+    MILLISECS_TIME m_CurrTime;
+    
     bool m_Paused;
 };
 /////////////////////////////////////////////////////////////////////////////////////////////
-#endif //
 
 /////////////////////////////////////////////////////////////////////////////////////////////
-#if defined WINTIME_MEASURE
+// Stopwatch high res time measure class
 class CHighResStopWatch
 {
 public:
@@ -136,123 +74,16 @@ public:
 
     float GetTime();
     unsigned int GetTicks();
-
+    
 protected:
-    __int64 m_StartTime;
-    __int64 m_CurrTime;
-    __int64 m_HighResFreq;
-
-    bool m_Paused;
-};
-/////////////////////////////////////////////////////////////////////////////////////////////
-#elif defined STDCHRONOLIB_TIME_MEASURE
-class CHighResStopWatch
-{
-public:
-    CHighResStopWatch(bool activate=true);
-    ~CHighResStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsPaused() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    std::chrono::high_resolution_clock::time_point m_StartTime;
-    std::chrono::high_resolution_clock::time_point m_CurrTime;
-
-    bool m_Paused;
-};
-/////////////////////////////////////////////////////////////////////////////////////////////
-#else // Use C time library
-class CHighResStopWatch
-{
-public:
-    CHighResStopWatch(bool bstart=true);
-    ~CHighResStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsPaused() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    clock_t m_StartTime;
-    clock_t m_CurrTime;
-
-    bool m_Paused;
-};
-#endif
-/////////////////////////////////////////////////////////////////////////////////////////////
-
-/*
-/////////////////////////////////////////////////////////////////////////////////////////////
-class CStopWatch
-{
-public:
-    CStopWatch(bool bstart=true);
-    ~CStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsActive() const;
-    bool IsHighRes() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    CLOCK_TIME m_StartTime;
-    CLOCK_TIME m_CurrTime;
-
-    bool m_Active;
-};
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-class CHighResStopWatch
-{
-public:
-    CHighResStopWatch(bool bstart=true);
-    ~CHighResStopWatch();
-
-    void Start();
-    void Stop();
-    void Reset();
-
-    bool IsActive() const;
-
-    float GetTime();
-    unsigned int GetTicks();
-
-protected:
-    MICROSEC_TIME m_StartTime;
-    MICROSEC_TIME m_CurrTime;
+    MICROSECS_TIME m_StartTime;
+    MICROSECS_TIME m_CurrTime;
 #ifdef WINTIME_MEASURE
-    MICROSEC_TIME m_HighResFreq;
+    MICROSECS_TIME m_HighResFreq;
 #endif //WINTIME_MEASURE
-    bool m_Active;
-};
 
-/////////////////////////////////////////////////////////////////////////////////////////////
-class CNanoStopWatch
-{
-public:
-    CNanoStopWatch();
-    ~CNanoStopWatch();
-
-protected:
+    bool m_Paused;
 };
-*/
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 #endif //__STOPWATCH_H__
