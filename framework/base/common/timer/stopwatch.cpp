b135e2b67dd0c2970ca011a998f494dd3d451236
Michal Lichojwan <mlichojwan@vewd.com>
2019-01-11 16:38:55 +0100

    Add nanoseconds measure stopwatch class
    New stopwatch class that measure time in nanoseconds.
    Added some warnings and remarks about using stopwatch classes.

--- framework/base/common/timer/stopwatch.cpp
+++ framework/base/common/timer/stopwatch.cpp
@@ -2,10 +2,17 @@
 // stopwatch.cpp
 //
 // Time measure stop watch classes defintions
+//
+// Warning: classes do not handle unsigned integers overflow. Ex. if in windows timer passed
+// In Windows after 49 days time function will be reseted and starts counting from beginning.
+// Nano stopwatch should be used for receiving small tick counts.
+// MinGW compiler on Windows C language time library has precision in milliseconds and
+// on Linux gcc compiler same library returns time in microseconds.
+// 
+// Remark: Think about merging all three classes into single template class.
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "stopwatch.h"
-
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 // Stopwatch time measure class
@@ -145,7 +152,6 @@ inline MICROSECS_TIME GetMicroSecsTime()
     return clock();
 #endif
 }
-
 /////////////////////////////////////////////////////////////////////////////////////////////
 inline float CalcMicroSecsTimeInFloat(MICROSECS_TIME curr_time, MICROSECS_TIME prev_time)
 {
@@ -247,3 +253,96 @@ unsigned int CHighResStopWatch::GetTicks()
     return uitime;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef STDCHRONOLIB_TIME_MEASURE
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Stopwatch nanoseconds time measure class
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+inline NANOSECS_TIME GetNanoSecsTime()
+{
+    return std::chrono::high_resolution_clock::now();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+inline double CalcNanoSecsTimeInDouble(NANOSECS_TIME curr_time, NANOSECS_TIME prev_time)
+{
+    std::chrono::duration<double> timespan = curr_time - prev_time;
+    std::chrono::nanoseconds nanosecs = std::chrono::duration_cast<std::chrono::nanoseconds>(timespan);
+    return (double)nanosecs.count() / 1000000000.0;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+inline unsigned int CalcNanoSecsTimeInUInt(NANOSECS_TIME curr_time, NANOSECS_TIME prev_time)
+{
+    std::chrono::duration<double> timespan = curr_time - prev_time;
+    std::chrono::nanoseconds nanosecs = std::chrono::duration_cast<std::chrono::nanoseconds>(timespan);
+    return (unsigned int) nanosecs.count();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+CNanoStopWatch::CNanoStopWatch(bool bactivate):
+m_Paused(true)
+{
+    Reset();
+    if(bactivate) Start();
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+CNanoStopWatch::~CNanoStopWatch()
+{
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void CNanoStopWatch::Start()
+{
+    if(!IsPaused()) return ;
+
+    m_Paused = false;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void CNanoStopWatch::Stop()
+{
+    if(IsPaused()) return ;
+
+    m_CurrTime = GetNanoSecsTime();
+    m_Paused = true;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+void CNanoStopWatch::Reset()
+{
+    m_StartTime = GetNanoSecsTime();
+    m_CurrTime = m_StartTime;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+bool CNanoStopWatch::IsPaused() const
+{
+    return m_Paused;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+double CNanoStopWatch::GetTime()
+{
+    if(m_Paused)
+    {        
+        double dbltime = CalcNanoSecsTimeInDouble(m_CurrTime, m_StartTime);     
+        return dbltime;
+    }
+    
+    NANOSECS_TIME currtime = GetNanoSecsTime();
+    double dbltime = CalcNanoSecsTimeInDouble(currtime, m_StartTime);
+    return dbltime;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+unsigned int CNanoStopWatch::GetTicks()
+{
+    if(m_Paused)
+    {        
+        unsigned int uitime = CalcNanoSecsTimeInUInt(m_CurrTime, m_StartTime);
+        return uitime;
+    }
+    
+    NANOSECS_TIME currtime = GetNanoSecsTime();
+    unsigned int uitime = CalcNanoSecsTimeInUInt(currtime, m_StartTime);
+    return uitime;
+}
+/////////////////////////////////////////////////////////////////////////////////////////////
+#endif //STDCHRONOLIB_TIME_MEASURE
