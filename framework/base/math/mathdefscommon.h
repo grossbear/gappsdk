a24d50808989c01015619e0c221bb04fa90c2933
grossbear <lichojwan@gmail.com>
2019-05-09 17:20:15 +0200

    Create mathdefscommon.h

---
+++ framework/base/math/mathdefscommon.h
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////////////
+// mathdefscommon.h
+//  
+// Header contains defined macros both for library and for external usage
+// File modified by Makefile script
+//  
+///////////////////////////////////////////////////////////////////////////////////////
+
+// Library uses it's own conversion from floating point number to integer and vice-versa
+//#define MATH_NO_FPU
+
+// Build classes instantiation and functions using following data types
+// MATH_DOUBLE_INST, MATH_LONG_DOUBLE_INST, MATH_FIXED_INST, MATH_HALF_INST
